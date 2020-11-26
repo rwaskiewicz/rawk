26026b60c5d96de38c594f446588cb8a544685ff
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2020-11-26 10:40:18 -0500

    Rough sketch of how I think the REPL will work
    - For now, forget filename
    - Also, assume they've written something like - awk '<PATTERN> <ACTION>' e.g. no data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,59 @@
+use clap::{App, Arg};
+use std::io;
+use std::io::Write;
+
 fn main() {
+    let matches = App::new("R-Awk")
+        .version("0.0.1")
+        .about("awk, implemented in Rust")
+        .arg(
+            Arg::with_name("file")
+                .short("f")
+                .long("file")
+                .takes_value(true)
+                .required(false),
+        )
+        .get_matches();
+
+    let file_name = matches.value_of("file");
+    match file_name {
+        None => {
+            run_prompt();
+        }
+        Some(s) => {
+            println!("TODO: Implement file parsing. Got file_name {}", s);
+        }
+    }
+
     println!("Hello, world!");
+}
+
+fn run_prompt() {
+    println!("R-AWK - a subset of awk written in Rust");
+
+    let mut awk_line = String::new();
+    let mut awk_input = String::new();
+
+    loop {
+        print!("RAWK > ");
+        io::stdout().flush().expect("Unable to flush STDOUT!");
+
+        io::stdin()
+            .read_line(&mut awk_line)
+            .expect("failed to get r-awk line");
+        print!("R-AWK Line: {}", awk_line);
+        while !awk_input.contains("STOP!") {
+            print!("Input Data >> ");
+            io::stdout().flush().expect("Unable to flush STDOUT!");
+
+            awk_input.clear();
+
+            io::stdin()
+                .read_line(&mut awk_input)
+                .expect("failed to get r-awk input!");
+            print!("Received Data: {}", awk_input);
+        }
+        awk_input.clear();
+        awk_line.clear();
+    }
 }
