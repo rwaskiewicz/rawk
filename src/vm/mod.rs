868c7923474c51468376f87a05b780c0d8637f57
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2021-02-13 14:30:52 -0500

    minor clean up

--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -104,22 +104,23 @@ impl VM {
         }
     }
 
+    /// Perform a relational comparison between two operators on the stack
+    ///
+    /// When two operands are compared, either string comparison or numeric comparison may be
+    /// used. This depends upon the attributes of the operands, according to the following
+    /// symmetric matrix:
+    ///         +----------------------------------------------
+    ///         |       STRING          NUMERIC         STRNUM
+    /// --------+----------------------------------------------
+    ///         |
+    /// STRING  |       string          string          string
+    ///         |
+    /// NUMERIC |       string          numeric         numeric
+    ///         |
+    /// STRNUM  |       string          numeric         numeric
+    /// --------+----------------------------------------------
+    /// [Source - GNU Awk Manual](https://www.gnu.org/software/gawk/manual/html_node/Variable-Typing.html)
     fn comparison_op(&mut self, op_code: &OpCode) {
-        // When two operands are compared, either string comparison or numeric comparison may be
-        // used. This depends upon the attributes of the operands, according to the following
-        // symmetric matrix:
-        //         +----------------------------------------------
-        //         |       STRING          NUMERIC         STRNUM
-        // --------+----------------------------------------------
-        //         |
-        // STRING  |       string          string          string
-        //         |
-        // NUMERIC |       string          numeric         numeric
-        //         |
-        // STRNUM  |       string          numeric         numeric
-        // --------+----------------------------------------------
-        // via https://www.gnu.org/software/gawk/manual/html_node/Variable-Typing.html
-
         let is_string_comparison =
             matches!(self.peek(0), Value::String(_)) || matches!(self.peek(1), Value::String(_));
 
@@ -129,12 +130,16 @@ impl VM {
         if is_string_comparison {
             self.string_comparison(op_code, &*a.str_value(), &*b.str_value());
         } else {
-            debug!("Hey a {}", a.num_value());
-            debug!("Hey b {}", b.num_value());
             self.numeric_comparison(op_code, a.num_value(), b.num_value());
         }
     }
 
+    /// Perform a relational comparison between two strings, and push the result onto the stack
+    ///
+    /// # Arguments
+    /// - `op_code` the operation to perform
+    /// - `a` the first argument of the comparison. Is placed on the left hand side of the expression
+    /// - `b` the second argument of the comparison. Is placed on the right hand side of the expression
     fn string_comparison(&mut self, op_code: &OpCode, a: &str, b: &str) {
         match *op_code {
             OpCode::GreaterEqual => {
@@ -179,10 +184,19 @@ impl VM {
                 }
                 self.stack.push(Value::Number(result))
             }
-            _ => panic!("Unknown op code given for comparison '{:?}'", op_code),
+            _ => panic!(
+                "Unknown op code given for string comparison '{:?}'",
+                op_code
+            ),
         }
     }
 
+    /// Perform a relational comparison between two numbers, and push the result onto the stack
+    ///
+    /// # Arguments
+    /// - `op_code` the operation to perform
+    /// - `a` the first argument of the comparison. Is placed on the left hand side of the expression
+    /// - `b` the second argument of the comparison. Is placed on the right hand side of the expression
     fn numeric_comparison(&mut self, op_code: &OpCode, a: f32, b: f32) {
         match *op_code {
             OpCode::GreaterEqual => {
