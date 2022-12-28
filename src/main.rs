22719c0c770f2eb9d932a7b85f86fed6994a372a
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2022-12-27 23:12:01 -0500

    chore(deps): upgrade clap to v4
    this commit pulls the generation of a clap `Command` entity to a
    separate function, in an effort to test the command being generated as a
    part of the v4 upgrade process, as outlined in
    https://github.com/clap-rs/clap/releases/tag/v4.0.0-rc.1. doing so
    allows us to use the `debug_assert()` function provided by clap in a new
    unit test to help programmatically verify (some) of the cli's
    configuration.
    
    the various `*_KEY` str values were hoisted to the outermost scope of
    the `main` entrypoint to allow for reuse. they were not refactored under
    a single namespace (e.g. an `enum`) to keep the commit smaller and more
    focused. this allows us to use the str's in multiple places throughout
    the file.
    
    the generation of `version` using the clap builder api has been removed,
    as it is no longer needed (and in fact errors in at runtime should we
    add it as-is without additional modifications to the `Command` entity).
    
    in order to differentiate as to whether the `-q` (quick) and `-k` (eval)
    flags have been set by the user or by clap, we can no longer check if
    either key is present (as it will always be present). we can however,
    determine if the user provided it using it's origin (the command line vs
    a default field). the generation of a `RuntimeConfig` has been updated
    accordingly to make thsi distinction.
    
    any remaining changes not expressly called out were done in accordance
    to the migration guide linked earlier in this commit msg

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{Arg, ArgMatches, Command};
+use clap::{parser::ValueSource, Arg, ArgAction, ArgMatches, Command};
 use env_logger::{Builder, Env};
 use log::LevelFilter;
 use rawk::runtime_config::RuntimeConfig;
@@ -13,6 +13,9 @@ enum TempAwkReadFileError {
 
 const PROGRAM_KEY: &str = "program";
 const PROGRAM_FILE_KEY: &str = "file";
+const QUICK_KEY: &str = "quick";
+const EVAL_KEY: &str = "eval";
+const FIELD_SEPARATOR_KEY: &str = "field_separator";
 
 fn main() -> Result<(), Box<dyn Error>> {
     Builder::from_env(Env::default().default_filter_or("info"))
@@ -20,89 +23,80 @@ fn main() -> Result<(), Box<dyn Error>> {
         .filter_module("rustyline", LevelFilter::Error)
         .init();
 
-    const VERSION_KEY: &str = "version";
-    const QUICK_KEY: &str = "quick";
-    const EVAL_KEY: &str = "eval";
-    const FIELD_SEPARATOR_KEY: &str = "field_separator";
+    let cmd = build_awk_cli_command();
+    let render_version = cmd.render_version();
+    let cmd_line_matches = cmd.get_matches();
 
+    if !cmd_line_matches.contains_id(PROGRAM_KEY) && !cmd_line_matches.contains_id(PROGRAM_FILE_KEY)
+    {
+        // clap will add a newline to the rendered version string for us
+        print!("{render_version}");
+        return Ok(());
+    }
+
+    let field_separator = cmd_line_matches
+        .get_one::<String>(FIELD_SEPARATOR_KEY)
+        .map(|separator| separator.to_string())
+        .unwrap_or_else(|| panic!("{} not configured for command line", FIELD_SEPARATOR_KEY));
+
+    let is_eval = cmd_line_matches
+        .value_source(EVAL_KEY)
+        .unwrap_or_else(|| panic!("{} not configured for command line", EVAL_KEY))
+        .eq(&ValueSource::CommandLine);
+    let is_quick = cmd_line_matches
+        .value_source(QUICK_KEY)
+        .unwrap_or_else(|| panic!("{} not configured for command line", QUICK_KEY))
+        .eq(&ValueSource::CommandLine);
+    let config: RuntimeConfig = RuntimeConfig::new(None, field_separator, is_eval, is_quick);
+
+    let program = get_awk_program(&cmd_line_matches);
+    rawk::run_program(&program, config);
+    Ok(())
+}
+
+fn build_awk_cli_command() -> Command {
     // https://www.gnu.org/software/gawk/manual/html_node/Options.html
-    let cmd_line_app = Command::new(env!("CARGO_PKG_NAME"))
+    Command::new(env!("CARGO_PKG_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
-        .about("awk, implemented in Rust");
-
-    let version_info = cmd_line_app.render_version();
-
-    let cmd_line_matches = cmd_line_app
-        .arg(
-            Arg::new(VERSION_KEY)
-                .short('V')
-                .long(VERSION_KEY)
-                .takes_value(false)
-                .required(false)
-                .help("Determine the current version of r-awk"),
-        )
+        .about("awk, implemented in Rust")
         .arg(
             Arg::new(PROGRAM_FILE_KEY)
                 .short('f')
                 .long(PROGRAM_FILE_KEY)
-                .takes_value(true)
+                .num_args(1)
                 .required(false)
-                .multiple_occurrences(true)
-                .number_of_values(1)
+                .action(ArgAction::Append)
                 .help("Runs an awk program"),
         )
         .arg(
-            Arg::new(PROGRAM_KEY).index(1), // note this is the first positional argument, not the first argument as a whole
+            // note this is the first positional argument relative to other positional arguments.
+            // it is not the first position in the argument list as a whole
+            Arg::new(PROGRAM_KEY).index(1),
         )
         .arg(
             // TODO: Remove this when `BEGIN` is implemented. We could use -w, but this is quicker
             Arg::new(QUICK_KEY)
                 .short('q')
                 .long(QUICK_KEY)
-                .takes_value(false)
-                .required(false)
+                .action(ArgAction::SetTrue)
                 .help("Runs a single line of awk code without data, then terminates"),
         )
         .arg(
             Arg::new(EVAL_KEY)
                 .short('k') // '-e' is taken already...
                 .long(EVAL_KEY)
-                .takes_value(false)
-                .required(false)
+                .action(ArgAction::SetTrue)
                 .help("Runs a single line of awk code, then terminates"),
         )
         .arg(
             Arg::new(FIELD_SEPARATOR_KEY)
                 .short('F')
-                .takes_value(true)
+                .num_args(1)
                 .required(false)
+                .action(ArgAction::Set)
                 .default_value(" ")
                 .help("Sets the field separator character/regex for parsing data"),
         )
-        .get_matches();
-
-    if !cmd_line_matches.is_present(PROGRAM_KEY) && !cmd_line_matches.is_present(PROGRAM_FILE_KEY) {
-        // clap handles version flags itself, use post-matching results to handle other cases where
-        // we only wish to print the version info
-        println!("{}", version_info.trim());
-        return Ok(());
-    }
-
-    let program = get_awk_program(&cmd_line_matches);
-
-    let field_separator = cmd_line_matches
-        .value_of(FIELD_SEPARATOR_KEY)
-        .map(|separator| separator.to_string())
-        .expect("awk file separator default is unspecified");
-
-    let config: RuntimeConfig = RuntimeConfig::new(
-        None,
-        field_separator,
-        cmd_line_matches.is_present(EVAL_KEY),
-        cmd_line_matches.is_present(QUICK_KEY),
-    );
-    rawk::run_program(&program, config);
-    Ok(())
 }
 
 /// Retrieve an awk program from the command line
@@ -118,7 +112,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 /// # Return value
 /// - The awk program to run
 fn get_awk_program(cmd_line_matches: &ArgMatches) -> String {
-    if let Some(provided_awk_filepaths) = cmd_line_matches.values_of(PROGRAM_FILE_KEY) {
+    if let Some(provided_awk_filepaths) = cmd_line_matches.get_many::<String>(PROGRAM_FILE_KEY) {
         provided_awk_filepaths
             .map(|awk_filepath| {
                 // TODO: Support for '-' as a special filename
@@ -129,6 +123,19 @@ fn get_awk_program(cmd_line_matches: &ArgMatches) -> String {
             })
             .collect()
     } else {
-        cmd_line_matches.value_of(PROGRAM_KEY).unwrap().into()
+        cmd_line_matches
+            .get_one::<String>(PROGRAM_KEY)
+            .cloned()
+            .unwrap()
+    }
+}
+
+#[cfg(test)]
+mod cli {
+    use crate::build_awk_cli_command;
+
+    #[test]
+    fn verify_cli() {
+        build_awk_cli_command().debug_assert();
     }
 }
