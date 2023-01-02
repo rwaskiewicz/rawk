7350d19e95d6c379e9d411e3f7c239688a189a85
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2023-01-01 20:37:44 -0500

    broken, contrarian example

--- tests/cli.rs
+++ tests/cli.rs
@@ -4,3 +4,37 @@ fn cli_tests() {
         .case("tests/cli/*.md")
         .case("tests/cli/*.toml");
 }
+
+pub mod utils;
+
+#[test]
+fn other_cli_tests() {
+    utils::CodeRunner::init()
+        .cli_options(vec!["--help"])
+        .expect_output(r#"awk, implemented in Rust
+
+Usage: rawk [OPTIONS] [program]
+
+Arguments:
+  [program]
+
+Options:
+  -f, --file <file>         Runs an awk program
+  -q, --quick               Runs a single line of awk code without data, then terminates
+  -k, --eval                Runs a single line of awk code, then terminates
+  -F <field_separator>      Sets the field separator character/regex for parsing data [default: " "]
+  -h, --help                Print help information
+  -V, --version             Print version information
+
+"#,
+        )
+        .assert()
+}
+
+#[test]
+fn other_cli_tests_2() {
+    utils::CodeRunner::init()
+        .expect_output(r#"rawk 0.1.0"#,
+        )
+        .assert()
+}
