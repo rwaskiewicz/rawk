8527126f5666bd4fa9e003ecd76fb086a2eab086
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2023-01-16 19:59:24 -0500

    format

--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -46,7 +46,10 @@ impl VM {
         let mut data_iter = all_data.iter();
         // prime the pump with the first set of data
         let mut data = data_iter.next();
-        self.globals.insert("NR".into(), Value::Number(self.globals.get("NR").unwrap().num_value() + 1.0));
+        self.globals.insert(
+            "NR".into(),
+            Value::Number(self.globals.get("NR").unwrap().num_value() + 1.0),
+        );
         loop {
             let instruction: OpCode = self.chunk.code[self.ip].code.clone();
             self.ip += 1;
@@ -69,7 +72,10 @@ impl VM {
                     }
 
                     data = data_iter.next();
-                    self.globals.insert("NR".into(), Value::Number(self.globals.get("NR").unwrap().num_value() + 1.0));
+                    self.globals.insert(
+                        "NR".into(),
+                        Value::Number(self.globals.get("NR").unwrap().num_value() + 1.0),
+                    );
                     if data.is_none() {
                         // we are out of records and are done running
                         break Ok(());
