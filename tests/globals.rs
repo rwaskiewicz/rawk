a048189941e715bb27faa94f1164acfe6663e419
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2023-01-22 14:29:25 -0500

    ignore and comment tests we cannot support yet

--- tests/globals.rs
+++ tests/globals.rs
@@ -47,13 +47,18 @@ NF 23"#,
     }
 
     #[test]
+    // TODO: Restore once field variable assignment is implemented
+    #[ignore]
     fn foo() {
-        // However, assigning to a nonexistent field (for example, $(NF+2)=5) shall increase the value of NF; create any intervening fields with the uninitialized value; and cause the value of $0 to be recomputed, with the fields being separated by the value of OFS.
+        // However, assigning to a nonexistent field (for example, $(NF+2)=5) shall increase the
+        // value of NF; create any intervening fields with the uninitialized value...
         utils::CodeRunner::init()
             .program("{ $(NF+1)=5; print NF; print $0; }")
             .stdin_data("hello world")
-            .expect_output(r#"3
-hello world 5"#)
+            .expect_output(
+                r#"3
+hello world 5"#,
+            )
             .assert();
     }
 
@@ -65,41 +70,45 @@ hello world 5"#)
     // obtained from an input line of "\t\t" when FS= '\t'. Historically, the comparison ($1<10) was
     // done numerically after evaluating $1 to the value zero.
 
-//     #[test]
-//     fn it_sets_nf_per_input_line() {
-//         utils::CodeRunner::init()
-//             .program("{ print NF; }")
-//             .stdin_data(
-//                 r#"hello world
-// i
-// come in
-// peace"#,
-//             )
-//             .expect_output(
-//                 r#"2
-// 1
-// 2
-// 1"#,
-//             )
-//             .assert();
-//     }
-//
-//     #[test]
-//     fn it_sets_nf_per_input_line_with_assign() {
-//         utils::CodeRunner::init()
-//             .program("{ if (NF == 2) { NF = 23; } print NF; }")
-//             .stdin_data(
-//                 r#"hello world
-// i
-// come in
-// peace"#,
-//             )
-//             .expect_output(
-//                 r#"23
-// 1
-// 23
-// 1"#,
-//             )
-//             .assert();
-//     }
+    #[test]
+    // TODO: Restore once we support multiline input in the test runner
+    #[ignore]
+    fn it_sets_nf_per_input_line() {
+        utils::CodeRunner::init()
+            .program("{ print NF; }")
+            .stdin_data(
+                r#"hello world
+i
+come in
+peace"#,
+            )
+            .expect_output(
+                r#"2
+1
+2
+1"#,
+            )
+            .assert();
+    }
+
+    #[test]
+    // TODO: Restore once we support multiline input in the test runner
+    #[ignore]
+    fn it_sets_nf_per_input_line_with_assign() {
+        utils::CodeRunner::init()
+            .program("{ if (NF == 2) { NF = 23; } print NF; }")
+            .stdin_data(
+                r#"hello world
+i
+come in
+peace"#,
+            )
+            .expect_output(
+                r#"23
+1
+23
+1"#,
+            )
+            .assert();
+    }
 }
